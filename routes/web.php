217e94644948872926ca8bd5aaa4fa22113c45ca
Hameed <apple@Apples-MacBook-Pro.local>
2023-06-07 14:34:44 +0500

    task for logout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,9 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/blogs', [BlogController::class, 'index']);
     Route::get('/blogs/create', [BlogController::class, 'create'])->name('blog.create');
     Route::post('/blogs/store', [BlogController::class, 'store'])->name('blog.add');
+    Route::get('/blogs', [BlogController::class, 'index']);
 
 });
 
